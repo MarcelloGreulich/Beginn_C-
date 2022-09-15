91c36fc155c3126446c96dcb7f575011c6f6ec7c
Marcello, Greulich <marcello.greulich@tecalliance.net>
2022-09-15 10:18:32 +0200

    add else if statement to code

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -13,16 +13,19 @@ namespace beginning
             //Ask and store age
             Console.WriteLine("Wie alt bist du?");
             int age = Convert.ToInt32(Console.ReadLine());
-            //test if user is 18+ 
-            if(age < 18)
-            {
-                Console.WriteLine("Du kommst hier net rein!");
-            }
-            else
+            //test if user is 18+ or 28+
+            if(age >= 28)
             {
                 Console.WriteLine("Viel Spaß");
             }
-
+            else if(age >= 18)
+            {
+                Console.WriteLine("Perso bidde!");
+            }
+            else
+            {
+                Console.WriteLine("Du kommst hier net rein!");
+            }
             //end Programm
             Console.ReadKey();
 
